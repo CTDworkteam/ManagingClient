5d69df48820eb681c44011e1dc4951b06a8121e3
yd1996 <834269940@qq.com>
2014-12-17 00:07:28 +0800

    mmm

--- utility/enumType/BillType.java
+++ utility/enumType/BillType.java
@@ -1,5 +1,20 @@
 package enumType;
 
 public enum BillType {
-
+	PurchaseBill,
+	PurchaseReturnBill,
+	SalesBill,
+	SalesReturnBill,
+	StockOverflowBill,
+	StockUnderflowBill,
+	StockGiftBill,
+	StockNoticeBill,
+	FinancePaymentBill,
+	FinanceReciptBill,
+	FinanceExpenseBill,
+	GiftStrategy,
+	GiftBasedOnTotalMoney,
+	DiscountStrategy,
+	VoucherStrategy,
+	CombinationStrategy
 }
