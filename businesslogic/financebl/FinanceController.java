76fb906ff466f666d4bb0dbad2d48236c5614100
DongCanRola <dongcanthis@outlook.com>
2014-12-17 22:47:59 +0800

    comdify

--- businesslogic/financebl/FinanceController.java
+++ businesslogic/financebl/FinanceController.java
@@ -1,5 +1,7 @@
 package financebl;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
 import vo.PaymentVO;
 import vo.ReciptVO;
 import enumType.ResultMessage;
@@ -26,6 +28,38 @@ public class FinanceController implements FinanceBLService{
 	}
 	public ResultMessage executeReturnBill(PaymentVO bill) {
 		return finance.executeReturnBill(bill);
+	}
+	@Override
+	public ReciptVO findRecipt(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public PaymentVO findPayment(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ArrayList<ReciptVO> findRecipts(GregorianCalendar before,
+			GregorianCalendar after) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ArrayList<PaymentVO> findPayments(GregorianCalendar before,
+			GregorianCalendar after) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ArrayList<ReciptVO> getAllRecipts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ArrayList<PaymentVO> getAllPayments() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
