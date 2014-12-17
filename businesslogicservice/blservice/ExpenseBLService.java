eb7cfe656c08f5fac73e0823935e40920b878abd
yd1996 <834269940@qq.com>
2014-12-17 21:52:13 +0800

    jjj

--- businesslogicservice/blservice/ExpenseBLService.java
+++ businesslogicservice/blservice/ExpenseBLService.java
@@ -1,14 +1,8 @@
 package blservice;
 import vo.*;
 import enumType.*;
-<<<<<<< HEAD
-public interface ExpenseBLService {
-//	public ResultMessage add(ExpenseVO vo);
-//	public ResultMessage delete(ExpenseVO vo);   
-=======
 import java.util.*;
 public interface ExpenseBLService { 
->>>>>>> refs/remotes/origin/master
 	public ResultMessage save(ExpenseVO vo);
 	public ResultMessage send(ExpenseVO vo);
 	public ExpenseVO find(String id);
--- businesslogicservice/blservice/UserBLService.java
+++ businesslogicservice/blservice/UserBLService.java
@@ -10,8 +10,4 @@ public interface UserBLService {
 	public UserVO findUser(long id);
 	public ResultMessage updateUser(UserVO user);
 	public UserListVO getAll();
-<<<<<<< HEAD
-=======
-	
->>>>>>> refs/remotes/origin/master
 }
