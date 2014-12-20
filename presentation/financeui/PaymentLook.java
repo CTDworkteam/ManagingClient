d58155e25b2c30841dd7f7499f08ea04b32abd6c
DongCanRola <dongcanthis@outlook.com>
2014-12-20 14:16:55 +0800

    comdift

---
+++ presentation/financeui/PaymentLook.java
@@ -0,0 +1,79 @@
+package financeui;
+import javax.swing.*;
+import vo.*;
+import java.util.*;
+
+/*
+ * 查看所选付款单界面
+ */
+public class PaymentLook {
+	
+	JFrame frame=new JFrame();
+	JPanel panel=new JPanel();
+	JLabel label=new JLabel("付款单");
+	String[][] data;
+	
+	public void go(PaymentVO vo){
+		
+		String id=vo.getId();
+		//单据状态
+		String state;
+		if(vo.isPassed()==true){
+			state="已通过";
+		}else{
+			state="未通过";
+		}
+		
+		String client=vo.getClient();
+		String operator=vo.getOperator();
+		ArrayList<FinanceItemVO> items=vo.getList();
+		double total=vo.getTotal();
+		
+		JLabel labelID=new JLabel(id);
+		JLabel labelState=new JLabel(state);
+		JLabel labelClient=new JLabel("客户："+client);
+		JLabel labelOperator=new JLabel("操作员："+operator);
+		JLabel labelTotal=new JLabel("总计："+Double.toString(total));
+		
+		//转账信息列表设置
+		String[] heading={"帐户名","转账金额","备注"};
+		for(int t=0;t<items.size();t++){
+			data[t][0]=items.get(t).getAccount();
+			data[t][1]=Double.toString(items.get(t).getMoney());
+			data[t][2]=items.get(t).getNote();
+		}
+		JTable table=new JTable(data,heading);
+		table.setEnabled(false);
+		table.getColumnModel().getColumn(2).setPreferredWidth(300);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		JScrollPane scroller=new JScrollPane(table);
+		
+		int wide=frame.getToolkit().getScreenSize().width;
+		int high=frame.getToolkit().getScreenSize().height;
+		
+		frame.setSize(wide/3,high-300);
+		frame.setLocation(wide/3,high-150);
+		
+		panel.setLayout(null);
+		
+		label.setBounds(wide/12,30,100,25);
+		labelID.setBounds(wide/12,65,200,25);
+		labelState.setBounds(wide/4,120,100,25);
+		labelClient.setBounds(20,150,150,25);
+		labelOperator.setBounds(300,150,150,25);
+		scroller.setBounds(50,190,300,300);
+		labelTotal.setBounds(wide/4,400,100,25);
+		
+		panel.add(label);
+		panel.add(labelID);
+		panel.add(labelState);
+		panel.add(labelClient);
+		panel.add(labelOperator);
+		panel.add(scroller);
+		panel.add(labelTotal);
+		
+		frame.getContentPane().add(panel);
+		frame.setVisible(true);
+	}
+
+}
