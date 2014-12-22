5897a9cc2508220d8a16c06cd12065cc79417b67
DongCanRola <dongcanthis@outlook.com>
2014-12-22 18:49:37 +0800

    comdify

--- presentation/financeui/PaymentConfirm.java
+++ presentation/financeui/PaymentConfirm.java
@@ -1,26 +1,68 @@
 package financeui;
-import javax.swing.*;
-import vo.*;
+import java.util.ArrayList;
 
+import javax.swing.*;
+import java.awt.event.*;
+
+import vo.*;
+import enumType.ResultMessage;
+import financebl.FinanceController;
+import confirmui.*;
+
+/*
+ * 付款单确认界面
+ */
 public class PaymentConfirm {
 	
-	JFrame frame;
-	JPanel panel;
+	JFrame frame=new JFrame();
+	JPanel panel=new JPanel();
+	
+	String[][] data;
+	
+	ArrayList<PaymentVO> payment=new ArrayList<PaymentVO>();
 	
 	public void go(PaymentVO vo){
-		frame=new JFrame();
-		panel=new JPanel();
+		
+		payment.add(vo);
 		
 		int wide=frame.getToolkit().getScreenSize().width;
 		int high=frame.getToolkit().getScreenSize().height;
 		
 		frame.setTitle("财务人员——财务管理——付款单生成");
 		
-		JTextArea text=new JTextArea();
-		JScrollPane scroller=new JScrollPane(text);
-		text.setLineWrap(true);
-		scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		String id=vo.getId();
+		//单据状态
+		String state;
+		if(vo.isPassed()==true){
+			state="已通过";
+		}else{
+			state="未通过";
+		}
+		
+		String client=vo.getClient();
+		String operator=vo.getOperator();
+		ArrayList<FinanceItemVO> items=vo.getList();
+		double total=vo.getTotal();
+		
+		JLabel labelID=new JLabel(id);
+		JLabel labelState=new JLabel(state);
+		JLabel labelClient=new JLabel("客户："+client);
+		JLabel labelOperator=new JLabel("操作员："+operator);
+		JLabel labelTotal=new JLabel("总计："+Double.toString(total));
+		
+		//转账信息列表设置
+		String[] heading={"帐户名","转账金额","备注"};
+		for(int t=0;t<items.size();t++){
+			data[t][0]=items.get(t).getAccount();
+			data[t][1]=Double.toString(items.get(t).getMoney());
+			data[t][2]=items.get(t).getNote();
+		}
+		JTable table=new JTable(data,heading);
+		table.setEnabled(false);
+		table.getColumnModel().getColumn(2).setPreferredWidth(300);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		JScrollPane scroller=new JScrollPane(table);
+		
 		JButton buttonCancel=new JButton("取消");
 		JButton buttonComdify=new JButton("修改");
 		JButton buttonSubmit=new JButton("提交");
@@ -28,17 +70,55 @@ public class PaymentConfirm {
 		frame.setSize(wide/2+100,high/2+200);
 		frame.setLocation(wide/4,high/6);
 		panel.setLayout(null);
-		scroller.setBounds(130,3,500,high/2+180);
+		labelID.setBounds(280,65,200,25);
+		labelState.setBounds(380,120,100,25);
+		labelClient.setBounds(130,150,150,25);
+		labelOperator.setBounds(300,150,150,25);
+		scroller.setBounds(180,190,300,300);
+		labelTotal.setBounds(500,400,100,25);
 		buttonCancel.setBounds(690,90,65,25);
 		buttonComdify.setBounds(690,135,65,25);
 		buttonSubmit.setBounds(690,180,65,25);
 		
+		panel.add(labelID);
+		panel.add(labelState);
+		panel.add(labelClient);
+		panel.add(labelOperator);
 		panel.add(scroller);
+		panel.add(labelTotal);
 		panel.add(buttonCancel);
 		panel.add(buttonComdify);
 		panel.add(buttonSubmit);
 		
 		frame.getContentPane().add(panel);
 		frame.setVisible(true);
+	}
+	
+	class cancelListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			frame.dispose();
+		}
+	}
+	
+	class comdifyListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			frame.dispose();
+		}
+	}
+	
+	class submitListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			FinanceController controller=new FinanceController();
+			ResultMessage result=controller.sendPayment(payment);
+			if(result==ResultMessage.Failure){
+				Runnable r=new Confirmui("提交失败");
+				Thread thread=new Thread(r);
+				thread.start();
+			}else{
+				Runnable r=new Confirmui("提交成功");
+				Thread thread=new Thread(r);
+				thread.start();
+			}
+		}
 	}
 }
--- presentation/strategyui/StrategyPriceui.java
+++ presentation/strategyui/StrategyPriceui.java
@@ -1,34 +1,53 @@
 package strategyui;
 import javax.swing.*;
+
+import vo.GiftBasedOnTotalMoneyVO;
+import vo.GiftBasedOnTotalMoneyVO.GBOTMItemVO;
+
 import java.awt.event.*;
+import java.util.*;
+import confirmui.*;
+import strategybl.StrategyController;
+import enumType.ResultMessage;
 
 public class StrategyPriceui {
 	
 	public JPanel panel=new JPanel();
 	
+	JTextField fieldLow;
+	JTextField fieldHigh;
+	JTextField fieldName;
+	JTextField fieldModel;
+	JTextField fieldCount;
+	JTextField fieldVoucher;
+	
+	ArrayList<GBOTMItemVO> item=new ArrayList<GBOTMItemVO>();
+	
 	public StrategyPriceui(){
-		JLabel labelPrice=new JLabel("总价",JLabel.RIGHT);
-		JTextField fieldPrice=new JTextField();
+		JLabel labelLow=new JLabel("价格下限",JLabel.RIGHT);
+		fieldLow=new JTextField();
+		JLabel labelHigh=new JLabel("价格上限",JLabel.RIGHT);
+		fieldHigh=new JTextField();
 		JLabel labelName=new JLabel("商品名",JLabel.RIGHT);
-		JComboBox<String> boxName=new JComboBox<String>();
-		boxName.setEditable(true);
-		JLabel labelNumber=new JLabel("商品编号",JLabel.RIGHT);
-		JComboBox<String> boxNumber=new JComboBox<String>();
-		boxNumber.setEditable(true);
-		JLabel labelCount=new JLabel("商品数量",JLabel.RIGHT);
-		JTextField fieldCount=new JTextField();
+		fieldName=new JTextField();
+		JLabel labelModel=new JLabel("型号",JLabel.RIGHT);
+		fieldModel=new JTextField();
+		JLabel labelCount=new JLabel("数量",JLabel.RIGHT);
+		fieldCount=new JTextField();
 		JLabel labelVoucher=new JLabel("代金券",JLabel.RIGHT);
-		JTextField fieldVoucher=new JTextField();
+		fieldVoucher=new JTextField();
 		JButton buttonAdd=new JButton("加入");//加入一项赠品
 		JButton buttonConfirmItem=new JButton("确定");//确定一项策略制定
 		
 		panel.setLayout(null);
-		labelPrice.setBounds(30,3,100,25);
-		fieldPrice.setBounds(140,3,50,25);
+		labelLow.setBounds(30,3,80,25);
+		fieldLow.setBounds(115,3,50,25);
+		labelHigh.setBounds(175,3,80,25);
+		fieldHigh.setBounds(260,3,50,25);
 		labelName.setBounds(3,38,75,25);
-		boxName.setBounds(93,38,75,25);
-		labelNumber.setBounds(3,70,75,25);
-		boxNumber.setBounds(93,70,75,25);
+		fieldName.setBounds(93,38,75,25);
+		labelModel.setBounds(3,70,75,25);
+		fieldModel.setBounds(93,70,75,25);
 		labelCount.setBounds(3,102,75,25);
 		fieldCount.setBounds(93,102,75,25);
 		labelVoucher.setBounds(153,70,75,25);
@@ -36,12 +55,14 @@ public class StrategyPriceui {
 		buttonAdd.setBounds(93,140,65,25);
 		buttonConfirmItem.setBounds(243,175,65,25);
 		
-		panel.add(labelPrice);
-		panel.add(fieldPrice);
+		panel.add(labelLow);
+		panel.add(fieldLow);
+		panel.add(labelHigh);
+		panel.add(fieldHigh);
 		panel.add(labelName);
-		panel.add(boxName);
-		panel.add(labelNumber);
-		panel.add(boxNumber);
+		panel.add(fieldName);
+		panel.add(labelModel);
+		panel.add(fieldModel);
 		panel.add(labelCount);
 		panel.add(fieldCount);
 		panel.add(labelVoucher);
@@ -50,16 +71,53 @@ public class StrategyPriceui {
 		panel.add(buttonConfirmItem);
 	}
 	
-	class boxNameListener implements ActionListener{
+	class addListener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-			
+			if(fieldName.getText()!=null&&fieldModel.getText()!=null&&fieldCount.getText()!=null){
+				GiftBasedOnTotalMoneyVO.GBOTMItemVO vo=new GiftBasedOnTotalMoneyVO().new GBOTMItemVO();
+				vo.setCommodity(fieldName.getText());
+				vo.setModel(fieldModel.getText());
+				vo.setNumber(Integer.parseInt(fieldCount.getText()));
+				item.add(vo);
+			}else{
+				Runnable r=new Confirmui("");
+				Thread t=new Thread(r);
+				t.start();
+			}
 		}
 	}
 	
-	class boxNumberListener implements ActionListener{
+	class confirmListener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-			
+			StrategyController controller=new StrategyController();
+			GregorianCalendar date=new GregorianCalendar();
+			String id=controller.getNewGiftBasedOnTotalMoney(date);
+			if(item.get(0)!=null){
+				GiftBasedOnTotalMoneyVO gift=new GiftBasedOnTotalMoneyVO();
+				gift.setId(id);
+				gift.setList(item);
+				gift.setLower(Double.parseDouble(fieldLow.getText()));
+				gift.setUpper(Double.parseDouble(fieldHigh.getText()));
+				ResultMessage result=controller.setGiftBasedOnTotalMoney(gift);
+				if(result==ResultMessage.Failure){
+					Runnable r=new Confirmui("未能成功制定针对总价的促销策略");
+					Thread t=new Thread(r);
+					t.start();
+				}else{
+					Runnable r=new Confirmui("成功制定针对总价的促销策略");
+					Thread t=new Thread(r);
+					t.start();
+					
+				}
+			}else{
+				if(fieldVoucher.getText()!=null){
+					
+				}else{
+					Runnable r=new Confirmui("");
+					Thread t=new Thread(r);
+					t.start();
+				}
+			}
 		}
 	}
-
 }
