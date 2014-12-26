8ba1520502e20479800bae530cb54ae0ac7c8216
yd1996 <834269940@qq.com>
2014-12-26 16:55:20 +0800

    66666

--- dataservice/dataservice/ClientDataService.java
+++ dataservice/dataservice/ClientDataService.java
@@ -3,7 +3,17 @@ import po.*;
 
 import java.util.*;
 public interface ClientDataService {
+	/**
+	 * @author 殷迪
+	 * @param po 需要添加的新的客户信息的数据对象
+	 * 在系统数据中没有重名或者重ID的客户ClientPO数据对象的前置条件下，向系统数据中加入一个新的ClientPO数据对象
+	 */
 	public void insert(ClientPO po);
+	/**
+	 * @author 殷迪
+	 * @param po 需要删除的旧的客户信息的数据对象
+	 * 在系统数据中存在相应的ClientPO数据对象的前置条件下，删除系统数据中
+	 */
     public void delete(ClientPO po);
     public boolean contain(String name);
     public boolean contain(long id);
