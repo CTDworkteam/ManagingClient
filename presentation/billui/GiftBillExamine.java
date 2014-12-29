c474709435df0eab0d16dcf6b36f83dd1085d37b
DongCanRola <dongcanthis@outlook.com>
2014-12-29 10:44:07 +0800

    gui add

---
+++ presentation/billui/GiftBillExamine.java
@@ -0,0 +1,137 @@
+package billui;
+import javax.swing.*;
+
+import java.awt.Dimension;
+import java.awt.event.*;
+import java.util.*;
+
+import enumType.ResultMessage;
+import confirmui.*;
+import billbl.Bill;
+import vo.*;
+import vo.GiftBillVO.GiftBillItemVO;
+
+/*
+ * 单个库存赠送单审批界面
+ */
+public class GiftBillExamine {
+	
+	JFrame frame=new JFrame();
+	JPanel panel=new JPanel();
+	
+	JLabel label=new JLabel("库存赠送单");
+	
+	JTextField fieldClient;
+	JTextField fieldStorehouse;
+	JTextField fieldOperator;
+	JComboBox<String> boxState;
+	
+	String[][] data=new String[3][3];
+	JTable table;
+	
+	GiftBillVO vo;
+
+	public void go(GiftBillVO vo){
+		
+		this.vo=vo;
+		
+		int wide=frame.getToolkit().getScreenSize().width;
+		int high=frame.getToolkit().getScreenSize().height;
+		
+		frame.setSize(wide/2-250,high*3/4);
+		frame.setLocation(wide/4,high/8);
+		
+		String[] heading={"编号","型号","数量"};
+		ArrayList<GiftBillItemVO> items=new ArrayList<GiftBillItemVO>();
+		for(int t=0;t<items.size();t++){
+			data[t][0]=items.get(t).getCommodity();
+			data[t][1]=items.get(t).getModel();
+			data[t][2]=Integer.toString(items.get(t).getNumber());
+		}
+		table=new JTable(data,heading);
+		table.setPreferredScrollableViewportSize(new Dimension(460,60));
+		JScrollPane scroller=new JScrollPane(table);
+		
+		JLabel labelID=new JLabel(vo.getId());
+		JLabel labelClient=new JLabel("客户：");
+		fieldClient=new JTextField();
+		fieldClient.setText(vo.getClient());
+		JLabel labelStorehouse=new JLabel("仓库：");
+		fieldStorehouse=new JTextField();
+		fieldStorehouse.setText(vo.getStorehouse());
+		JLabel labelOperator=new JLabel("操作员：");
+		fieldOperator=new JTextField();
+		fieldOperator.setText(vo.getOperator());
+		boxState=new JComboBox<String>();
+		boxState.addItem("未通过");
+		boxState.addItem("通过");
+		JButton button=new JButton("确定");
+		
+		button.addActionListener(new buttonListener());
+		
+		panel.setLayout(null);
+		
+		label.setBounds(wide/7+20,30,100,50);
+		labelID.setBounds(wide/7+70,110,200,25);
+		labelClient.setBounds(70,155,60,25);
+		fieldClient.setBounds(130,155,80,25);
+		labelStorehouse.setBounds(70,200,60,25);
+		fieldStorehouse.setBounds(130,200,80,25);
+		labelOperator.setBounds(300,200,60,25);
+		fieldOperator.setBounds(360,200,80,25);
+		scroller.setBounds(70,260,400,100);
+		boxState.setBounds(350,390,80,25);
+		button.setBounds(wide/7+20,490,65,25);
+		
+		panel.add(label);
+		panel.add(labelID);
+		panel.add(labelClient);
+		panel.add(fieldClient);
+		panel.add(labelStorehouse);
+		panel.add(fieldStorehouse);
+		panel.add(labelOperator);
+		panel.add(fieldOperator);
+		panel.add(scroller);
+		panel.add(boxState);
+		panel.add(button);
+		
+		frame.getContentPane().add(panel);
+		frame.setVisible(true);
+	}
+	
+	class buttonListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			//计数赠品种数
+			int size=0;
+			while(table.getValueAt(size, 0)!=null){
+				size++;
+			}
+			
+			ArrayList<GiftBillItemVO> gifts=new ArrayList<GiftBillItemVO>();
+			for(int t=0;t<size;t++){
+				GiftBillVO.GiftBillItemVO theItem=new GiftBillVO().new GiftBillItemVO((String)table.getValueAt(t, 0),(String)table.getValueAt(t, 1),(int)table.getValueAt(t, 2));
+				gifts.add(theItem);
+			}
+			
+			GiftBillVO theBill=new GiftBillVO(vo.getId(),fieldClient.getText(),fieldStorehouse.getText(),fieldOperator.getText(),gifts);
+			String state=(String)boxState.getSelectedItem();
+			if(state=="未通过"){
+				theBill.setPassed(false);
+			}else{
+				theBill.setPassed(true);
+			}
+			
+			Bill bill=new Bill();
+			ResultMessage result=bill.approveGiftBill(theBill);
+			String message="";
+			if(result==ResultMessage.Failure){
+				message="操作失败";
+			}else{
+				message="成功审批";
+			}
+			Runnable r=new Confirmui(message);
+			Thread thread=new Thread(r);
+			thread.start();
+		}
+	}
+}
---
+++ presentation/financecheckui/DetailListCheck.java
@@ -0,0 +1,272 @@
+package financecheckui;
+import javax.swing.*;
+
+import java.awt.event.*;
+import java.util.*;
+
+import financecheckbl.FinanceCheckController;
+import commoditybl.CommodityController;
+import clientbl.ClientController;
+import userbl.UserController;
+import vo.*;
+import vo.DetailListVO.DetailListItemVO;
+import confirmui.*;
+
+/*
+ * 销售明细表审批界面
+ */
+public class DetailListCheck {
+
+	JPanel panel=new JPanel();
+	
+	JTextField fieldStart;
+	JTextField fieldEnd;
+	JComboBox<String> boxCommodity;
+	JComboBox<String> boxClient;
+	JComboBox<String> boxOperator;
+	JTextField fieldStorehouse;
+	
+	ArrayList<CommodityVO> commodities;
+	ArrayList<ClientVO> clients;
+	ArrayList<UserVO> operators;
+	
+	public DetailListCheck(){
+		
+		JLabel labelStart=new JLabel("开始时间：");
+		fieldStart=new JTextField();
+		fieldStart.setText("年/月/日");
+		JLabel labelEnd=new JLabel("结束时间：");
+		fieldEnd=new JTextField();
+		fieldEnd.setText("年/月/日");
+		
+		JLabel labelCommodity=new JLabel("商品名：");
+		boxCommodity=new JComboBox<String>();
+		boxCommodity.addItem(null);
+		CommodityController commodityController=new CommodityController();
+		commodities=commodityController.getAllCommodity();
+		for(int t=0;t<commodities.size();t++){
+			boxCommodity.addItem(commodities.get(t).getName());
+		}
+		
+		JLabel labelClient=new JLabel("客户：");
+		boxClient=new JComboBox<String>();
+		boxClient.addItem(null);
+		ClientController clientController=new ClientController();
+		clients=clientController.getList();
+		for(int t=0;t<clients.size();t++){
+			boxClient.addItem(clients.get(t).getName());
+		}
+		
+		JLabel labelOperator=new JLabel("操作员：");
+		boxOperator=new JComboBox<String>();
+		boxOperator.addItem(null);
+		UserController userController=new UserController();
+		operators=userController.getAll().getSalesmanager();
+		for(int t=0;t<operators.size();t++){
+			boxOperator.addItem(operators.get(t).getName());
+		}
+		
+		JLabel labelStorehouse=new JLabel("仓库");
+		fieldStorehouse=new JTextField();
+		JButton button=new JButton("查询");
+		
+		panel.setLayout(null);
+		
+		labelStart.setBounds(20,20,80,25);
+		fieldStart.setBounds(100,20,80,25);
+		labelEnd.setBounds(210,20,80,25);
+		fieldStart.setBounds(290,20,80,25);
+		labelStorehouse.setBounds(20,60,80,25);
+		fieldStorehouse.setBounds(100,60,80,25);
+		labelCommodity.setBounds(20,95,80,25);
+		boxCommodity.setBounds(100,95,80,25);
+		labelClient.setBounds(210,100,60,25);
+		boxClient.setBounds(270,100,80,25);
+		labelOperator.setBounds(370,100,60,25);
+		boxOperator.setBounds(430,100,80,25);
+		button.setBounds(350,200,65,65);
+		
+		panel.add(labelStart);
+		panel.add(fieldStart);
+		panel.add(labelEnd);
+		panel.add(fieldEnd);
+		panel.add(labelStorehouse);
+		panel.add(fieldStorehouse);
+		panel.add(labelCommodity);
+		panel.add(boxCommodity);
+		panel.add(labelClient);
+		panel.add(boxClient);
+		panel.add(labelOperator);
+		panel.add(boxOperator);
+		panel.add(button);
+	}
+	
+	class buttonListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			
+			if(fieldStart.getText().equals(null)&&fieldStorehouse.getText().equals(null)&&((String)boxCommodity.getSelectedItem()).equals(null)&&((String)boxClient.getSelectedItem()).equals(null)&&((String)boxOperator.getSelectedItem()).equals(null)){
+				Runnable r=new Confirmui("请填写查询条件");
+				Thread thread=new Thread(r);
+				thread.start();
+			}else{
+				FinanceCheckController controller=new FinanceCheckController();
+				
+				ArrayList<DetailListItemVO> listTime=new ArrayList<DetailListItemVO>();
+				if(fieldStart.getText()!=null||fieldEnd.getText()!=null){
+					if(fieldStart.getText()!=null&&fieldEnd.getText()!=null){
+						GregorianCalendar start=new GregorianCalendar();
+						String[] theStart=fieldStart.getText().split("/");
+						start.set(Integer.parseInt(theStart[0]), Integer.parseInt(theStart[1]), Integer.parseInt(theStart[2]));
+						GregorianCalendar end=new GregorianCalendar();
+						String[] theEnd=fieldEnd.getText().split("/");
+						end.set(Integer.parseInt(theEnd[0]), Integer.parseInt(theEnd[1]), Integer.parseInt(theEnd[2]));
+						listTime=controller.getDetailList(start, end).getList();
+					}else{
+						Runnable r=new Confirmui("请完整填写查询时间区间");
+						Thread thread=new Thread(r);
+						thread.start();
+					}
+				}
+				
+				String storehouse=fieldStorehouse.getText();
+				ArrayList<DetailListItemVO> listStorehouse=new ArrayList<DetailListItemVO>();
+				if(!storehouse.equals(null)){
+					listStorehouse=controller.getDetailListInStorehouse(storehouse).getList();
+				}
+				
+				String commodity=(String)boxCommodity.getSelectedItem();
+				ArrayList<DetailListItemVO> listCommodity=new ArrayList<DetailListItemVO>();
+				if(commodity!=null){
+					listCommodity=controller.getDetailList(commodity).getList();
+				}
+				
+				ArrayList<DetailListItemVO> listClient=new ArrayList<DetailListItemVO>();
+				if(boxClient.getSelectedItem()!=null){
+					ClientVO client=new ClientVO();
+					for(int t=0;t<clients.size();t++){
+						if(clients.get(t).getName()==(String)boxClient.getSelectedItem()){
+							client=clients.get(t);
+						}
+					}
+					listClient=controller.getDetailList(client).getList();
+				}
+				
+				ArrayList<DetailListItemVO> listOperator=new ArrayList<DetailListItemVO>();
+				if(!((String)boxOperator.getSelectedItem()).equals(null)){
+					UserVO operator=new UserVO();
+					for(int t=0;t<operators.size();t++){
+						if(operators.get(t).getName().equals((String)boxOperator.getSelectedItem())){
+							operator=operators.get(t);
+						}
+					}
+					listOperator=controller.getDetailList(operator).getList();
+				}
+				
+				ArrayList<DetailListItemVO> list=new ArrayList<DetailListItemVO>();
+				int sizeTime=listTime.size();
+				int sizeStorehouse=listStorehouse.size();
+				int sizeCommodity=listCommodity.size();
+				int sizeClient=listClient.size();
+				int sizeOperator=listOperator.size();
+				if(sizeTime>0){
+					list=listTime;
+				}else{
+					if(sizeStorehouse>0){
+						list=listStorehouse;
+					}else{
+						if(sizeCommodity>0){
+							list=listCommodity;
+						}else{
+							if(sizeClient>0){
+								list=listClient;
+							}else{
+								list=listOperator;
+							}
+						}
+					}
+				}
+				
+				if(sizeTime>0){
+					for(int t=0;t<list.size();){
+						for(int k=0;k<sizeTime;k++){
+							if(list.get(t).equals(listTime.get(k))){
+								t++;
+								break;
+							}else{
+								if(k==sizeTime-1){
+									list.remove(t);
+								}
+							}
+						}
+					}
+				}
+				
+				if(sizeStorehouse>0){
+					for(int t=0;t<list.size();){
+						for(int k=0;k<sizeStorehouse;k++){
+							if(list.get(t).equals(listStorehouse.get(k))){
+								t++;
+								break;
+							}else{
+								if(k==sizeStorehouse-1){
+									list.remove(t);
+								}
+							}
+						}
+					}
+				}
+				
+				if(sizeCommodity>0){
+					for(int t=0;t<list.size();){
+						for(int k=0;k<sizeCommodity;k++){
+							if(list.get(t).equals(listCommodity.get(k))){
+								t++;
+								break;
+							}else{
+								if(k==sizeCommodity-1){
+									list.remove(t);
+								}
+							}
+						}
+					}
+				}
+				
+				if(sizeClient>0){
+					for(int t=0;t<list.size();){
+						for(int k=0;k<sizeClient;k++){
+							if(list.get(t).equals(listClient.get(k))){
+								t++;
+								break;
+							}else{
+								if(k==sizeClient-1){
+									list.remove(t);
+								}
+							}
+						}
+					}
+				}
+				
+				if(sizeOperator>0){
+					for(int t=0;t<list.size();){
+						for(int k=0;k<sizeOperator;k++){
+							if(list.get(t).equals(listClient.get(k))){
+								t++;
+								break;
+							}else{
+								if(k==sizeOperator-1){
+									list.remove(t);
+								}
+							}
+						}
+					}
+				}
+				
+				DetailListVO vo=new DetailListVO(fieldStart.getText(),fieldEnd.getText(),(String)boxCommodity.getSelectedItem(),(String)boxOperator.getSelectedItem(),(String)boxClient.getSelectedItem(),fieldStorehouse.getText(),list);
+				DetailListShow show=new DetailListShow();
+				show.go(vo);
+			}
+			
+			
+		}
+	}
+}
